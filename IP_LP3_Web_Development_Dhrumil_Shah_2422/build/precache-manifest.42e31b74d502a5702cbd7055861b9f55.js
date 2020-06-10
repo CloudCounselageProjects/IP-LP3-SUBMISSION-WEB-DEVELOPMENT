08743e9651bf7a406f9adf855b587a2143cd715d
Dhrumil <50771715+Dhrumil0310@users.noreply.github.com>
2020-06-10 20:24:33 +0530

    initial commit LP3 web development project

---
+++ IP_LP3_Web_Development_Dhrumil_Shah_2422/build/precache-manifest.42e31b74d502a5702cbd7055861b9f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157915900d55043efe6f54b3fcc546d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581abb852ebfc951d158",
+    "url": "/static/css/main.0db04347.chunk.css"
+  },
+  {
+    "revision": "26b30649c44b62b5c842",
+    "url": "/static/js/2.5262be5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5262be5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581abb852ebfc951d158",
+    "url": "/static/js/main.b06d2874.chunk.js"
+  },
+  {
+    "revision": "b98b23125fa51f654eea",
+    "url": "/static/js/runtime-main.8324554e.js"
+  },
+  {
+    "revision": "097840a2a79d31dfb78e13b2352ca7de",
+    "url": "/static/media/scene.097840a2.jpg"
+  }
+]);
